6bda3e5ab2fa87ed9bb5fe00a3baae62567c1bf0
Seshan Ravikumar <seshan10@me.com>
2023-11-01 22:35:43 -0400

    device specific services

--- src/modules/pl2/index.ts
+++ src/modules/pl2/index.ts
@@ -1,6 +1,7 @@
 import { config } from "../../index";
 import { runCmd } from "../../helpers/runCmd";
 import {log} from "../../logging";
+import { getProLinuxInfo } from "../../helpers/getProLinuxInfo";
 
 export async function setHostname() {
     // Set system hostname
@@ -17,7 +18,30 @@ export async function setSSHHostKeys() {
     log.info("Generating SSH host keys");
     await runCmd("ssh-keygen", ["-A"]);
 }
+export async function startDeviceSpecificServices() {
+    const prolinuxInfo = await getProLinuxInfo();
+    const SDM845Devices = [
+        "oneplus-enchilada",
+        "xiaomi-beryllium",
+    ]
+    // SDM845 Modem Support
+    if(SDM845Devices.includes(prolinuxInfo.deviceinfoCodename)) {
+        log.info("Starting SDM845 Support Services...");
+        await runCmd("systemctl", ["start", "qrtr-ns"]);
+        await runCmd("systemctl", ["start", "rmtfs"]);
+        await runCmd("systemctl", ["start", "pd-mapper"]);
+        await runCmd("systemctl", ["start", "tqftpserv"]);
+    }
 
+    if(prolinuxInfo.deviceinfoCodename === "pine64-pinephone" || prolinuxInfo.deviceinfoCodename === "pine64-pinephonepro") {
+        log.info("Starting Pinephone(Pro) Support Services...");
+        await runCmd("systemctl", ["start", "eg25-manager"]);
+    }
+
+}
+
+/* Boot-time setup */
 export async function loadPL2Module() {
     await setHostname();
+    await startDeviceSpecificServices();
 }
