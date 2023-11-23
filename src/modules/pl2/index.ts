25a7f0ca9d442b48cfcfed906c3e7ce5b0b4012c
Seshan Ravikumar <seshan10@me.com>
2023-11-22 22:51:16 -0500

    Modem changes
    start ModemManager from ProLinuxD, add msm-modem-uim-selection to ProLinuxD, and quick suspend patch for sdm845

--- src/modules/pl2/index.ts
+++ src/modules/pl2/index.ts
@@ -29,15 +29,22 @@ export async function startDeviceSpecificServices() {
     // SDM845 Modem Support
     if(SDM845Devices.includes(prolinuxInfo.deviceinfoCodename)) {
         log.info("Starting SDM845 Support Services...");
+        // append "test-quick-suspend-resume" argument to ExecStart for ModemManager /usr/lib/systemd/system/ModemManager.service
+        // making sure it doesn't already exist
+        await runCmd("/usr/bin/bash", ["-c", String.raw`if ! grep -q "test-quick-suspend-resume" /usr/lib/systemd/system/ModemManager.service; then sed -i 's/ExecStart=\/usr\/bin\/ModemManager/ExecStart=\/usr\/bin\/ModemManager --test-quick-suspend-resume/g' /usr/lib/systemd/system/ModemManager.service; fi`]);
+
         await runCmd("systemctl", ["start", "qrtr-ns"]);
         await runCmd("systemctl", ["start", "rmtfs"]);
+        await runCmd("systemctl", ["start", "msm-modem-uim-selection"]);
         await runCmd("systemctl", ["start", "pd-mapper"]);
         await runCmd("systemctl", ["start", "tqftpserv"]);
+        await runCmd("systemctl", ["start", "ModemManager"]);
     }
 
     if(prolinuxInfo.deviceinfoCodename === "pine64-pinephone" || prolinuxInfo.deviceinfoCodename === "pine64-pinephonepro") {
         log.info("Starting Pinephone(Pro) Support Services...");
         await runCmd("systemctl", ["start", "eg25-manager"]);
+        await runCmd("systemctl", ["start", "ModemManager"]);
     }
 
 }
